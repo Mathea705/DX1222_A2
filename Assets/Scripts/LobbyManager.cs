45683564404ce7702968e2777d8b602a21a9c255
Mathea705 <matheayesilin@gmail.com>
2026-02-01 04:30:26 +0800

    can join lobbies and it syncs YYA

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Unity.Netcode;
+using System.Collections.Generic;
 
 public class LobbyManager : NetworkBehaviour
 {
@@ -18,13 +19,27 @@ public class LobbyManager : NetworkBehaviour
 
     [SerializeField] private GameObject contentPosition;
 
+     [Header("Lobby Stuff")]
+
+    public string currLobby;
+
+   private Dictionary<string, int> lobbyCounts = new Dictionary<string, int>();
+
+
+
      [Header("Other")]
 
     [SerializeField] private int maxCharCount;
+
+    [SerializeField] private TextMeshProUGUI lobbyStatusText;
+
+    private const int maxPlayers = 2;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currLobby = null;
     }
 
     // Update is called once per frame
@@ -51,7 +66,7 @@ public class LobbyManager : NetworkBehaviour
         return true;
     }
 
-        GameObject CreateLobby(string lobbyName)
+        GameObject CreateLobby(string lobbyName, int playerCount)
     {
         GameObject newLobby = Instantiate(lobbyItemPrefab);
         newLobby.transform.SetParent(contentPosition.transform, false);
@@ -63,6 +78,13 @@ public class LobbyManager : NetworkBehaviour
 
         nameText.text = lobbyName;
 
+        Button joinButton = newLobby.transform.Find("JoinButton").GetComponent<Button>();
+        joinButton.onClick.AddListener(() => OnJoinButtonClicked(lobbyName));
+
+        newLobby.transform.Find("PlayerCountText").GetComponent<TextMeshProUGUI>().text = playerCount.ToString() + " / " + maxPlayers;
+
+
+
         return newLobby;
     }
 
@@ -70,22 +92,65 @@ public class LobbyManager : NetworkBehaviour
         [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
     void RequestCreateLobbyServerRpc(string lobbyName)
     {
-        CreateLobbyClientRpc(lobbyName);
+            if (!lobbyCounts.ContainsKey(lobbyName))
+        {
+            lobbyCounts[lobbyName] = 1; // +1 because host will automcailly join the lobby they make
+        
+        }
+
+        CreateLobbyClientRpc(lobbyName, lobbyCounts[lobbyName]);
     }
 
 
         [Rpc(SendTo.ClientsAndHost)]
-    void CreateLobbyClientRpc(string lobbyName)
+    void CreateLobbyClientRpc(string lobbyName, int playerCount)
     {
-        CreateLobby(lobbyName);
+        CreateLobby(lobbyName, playerCount);
+
     }
 
+        [Rpc(SendTo.Server)]
+    void RequestJoinLobbyServerRpc(string lobbyName)
+    {
+        if (!lobbyCounts.ContainsKey(lobbyName)) return;
 
+            if (lobbyCounts[lobbyName] >= maxPlayers) return;
+
+        lobbyCounts[lobbyName]++;
+
+        UpdateLobbyCountClientRpc(lobbyName, lobbyCounts[lobbyName]);
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    void UpdateLobbyCountClientRpc(string lobbyName, int newCount)
+    {
+        foreach (Transform lobby in contentPosition.transform)
+        {
+            var nameText = lobby.Find("LobbyNameText").GetComponent<TextMeshProUGUI>();
+
+            if (nameText.text == lobbyName)
+            {
+                lobby.Find("PlayerCountText").GetComponent<TextMeshProUGUI>().text = newCount.ToString() + " / " + maxPlayers;
+                if (newCount >= maxPlayers)
+                {
+                    lobby.Find("JoinButton").GetComponent<Button>().interactable = false;
+                }
+
+                break;
+            }
+        }
+
+        
+    }
 
     public void OnMakeLobbyButtonClicked()
     {
         if (!IsValidName(inputFieldText))
             return;
+
+            currLobby = inputFieldText.text;
+          lobbyStatusText.text = "Hosting lobby: " + currLobby;
+
 
         inputPanel.SetActive(false);
 
@@ -94,8 +159,33 @@ public class LobbyManager : NetworkBehaviour
 
      public void OnHostLobbyButtonClicked()
     {
+        if (currLobby != null)
+        {
+            lobbyStatusText.color = Color.red;
+            lobbyStatusText.text = "You are already in a lobby!";
+            return;
+        }
         inputPanel.SetActive(true);
     }
+
+    public void OnJoinButtonClicked(string lobbyName)
+    {
+        if (currLobby != null)
+        {
+            lobbyStatusText.color = Color.red;
+            lobbyStatusText.text = "You are already in a lobby!";
+            return;
+        }
+        else
+        {
+            RequestJoinLobbyServerRpc(lobbyName);
+            lobbyStatusText.text = "Joined lobby: " + lobbyName;
+            currLobby = lobbyName;
+            
+        }
+
+    }
+
 
    
 
